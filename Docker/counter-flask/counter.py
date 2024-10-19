4735be77f9dc061115d24fa269a03d1b5f93036a
guledali30 <guledali30@hotmail.com>
2024-10-19 01:56:08 +0100

    Updated counter.py file

--- Docker/counter-flask/counter.py
+++ Docker/counter-flask/counter.py
@@ -2,13 +2,13 @@ from flask import Flask
 import os
 import redis
 
-app = Flask(__name__)  # Define the Flask app
+app = Flask(__name__)  
 
-# Connect to Redis using the service name 'redis'
-redis_host = os.getenv('REDIS_HOST', 'redis')  # Default to 'redis' if the env variable is not set
+
+redis_host = os.getenv('REDIS_HOST', 'redis') 
 redis_client = redis.StrictRedis(host=redis_host, port=6379, db=0)
 
-# Optional: Test the connection
+
 try:
     redis_client.ping()
     print("Connected to Redis")
@@ -21,10 +21,10 @@ def welcome():
 
 @app.route('/count')
 def count():
-    count = redis_client.incr('visits')  # Change 'r' to 'redis_client'
+    count = redis_client.incr('visits')  
     return f'This page has been visited {count} times'
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5002)  # Use '0.0.0.0' here
+    app.run(host='0.0.0.0', port=5002)  
 
 
